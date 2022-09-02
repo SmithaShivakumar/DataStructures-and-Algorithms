d1cd375dd5bd1da26186733ae4b25213b4ec4749
ShivSmit <smithashivakumar@rocketmail.com>
2022-09-02 12:30:39 -0700

    Time: 149 ms (37.31%), Space: 15.5 MB (8.71%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -10,3 +10,15 @@ class Solution:
             else:
                 continue
         return
+    
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        for i in range(len(nums)):
+            diff = (target - nums[i])
+            if diff in numMap:
+                return(numMap[diff], i)
+            numMap[nums[i]] = i 
+        return
+'''
