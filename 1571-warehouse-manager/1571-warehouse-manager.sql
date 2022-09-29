c9506b01d45113bb5df786841a032bb8b5aad1d0
ShivSmit <smithashivakumar@rocketmail.com>
2022-09-29 14:10:49 -0700

    Time: 1062 ms (19.87%), Space: 0B (100.00%) - LeetHub

--- 1571-warehouse-manager/1571-warehouse-manager.sql
+++ 1571-warehouse-manager/1571-warehouse-manager.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 
 with temporaryTable (product_id, V) as 
-    (select product_id, (Width*Length*Height) as V from Products group by  product_id)
-    select name as warehouse_name, sum(V*W.units) as volume 
-    from Warehouse W, temporaryTable P 
-    where P.product_id = W.product_id 
-    group by W.name  
+(select product_id, (Width*Length*Height) as V from Products group by  product_id)
+select name as warehouse_name, sum(V*W.units) as volume 
+from Warehouse W, temporaryTable P 
+where P.product_id = W.product_id 
+group by W.name  
 
 
 
