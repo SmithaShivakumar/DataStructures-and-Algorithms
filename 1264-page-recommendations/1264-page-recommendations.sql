8a5dc6cabff2fe527c6bc0543d920dd0d778f70c
ShivSmit <smithashivakumar@rocketmail.com>
2022-10-05 10:42:03 -0700

    Time: 509 ms (78.77%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -3,14 +3,14 @@
 
 
 
-SELECT DISTINCT page_id AS recommended_page 
-FROM Likes
-WHERE user_id IN 
-(SELECT 
-     CASE 
-     WHEN user1_id=1 THEN user2_id 
-     WHEN user2_id=1 THEN user1_id 
-     ELSE '' END
-FROM Friendship WHERE user1_id=1 or user2_id=1)
-AND page_id NOT IN 
-(SELECT DISTINCT page_id FROM Likes WHERE user_id=1)
+select distinct page_id as recommended_page
+from
+(select
+case
+when user1_id=1 then user2_id
+when user2_id=1 then user1_id
+end as user_id
+from Friendship) a
+join Likes
+on a.user_id=Likes.user_id
+where page_id not in (select page_id from Likes where user_id=1)
